69dfa4eb253cd89350a822d650fdd10a29b7108c
Vladimir Larin <lvv0820@gmail.com>
2023-01-23 20:01:05 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Profile from './components/Profile/Profile';
+// import user from './components/Profile/user.json';
+
+// console.log({ user });
+// console.log('t6t', user);
+
+export function App() {
+  return (
+    <>
+      <h4>dsgh</h4>
+    </>
+  );
+}
