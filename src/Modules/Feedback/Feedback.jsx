107d9202a867fd6cf0009978cdd29ff7a09b2522
Vladimir Larin <lvv0820@gmail.com>
2023-01-26 00:24:17 +0200

    fin

--- src/Modules/Feedback/Feedback.jsx
+++ src/Modules/Feedback/Feedback.jsx
@@ -1,21 +1,20 @@
 import { Component } from 'react';
-// import FeedbackRenderBtn from './FeedbackRenderBtn';
+import FeedbackOptions from './FeedbackOptions';
 import Statistic from './Statistic';
-
-import styles from './feedback.module.scss';
+import FeedbackBlock from './FeedbackBlock/FeedbackBlock';
 
 class Feedback extends Component {
   state = {
-    good: 10,
-    neutral: 2,
+    good: 0,
+    neutral: 0,
     bad: 0,
   };
 
-  changeState(feedback) {
+  changeState = feedback => {
     this.setState(stateValues => {
       return { [feedback]: stateValues[feedback] + 1 };
     });
-  }
+  };
 
   countTotalFeedback() {
     const { good, neutral, bad } = this.state;
@@ -33,45 +32,29 @@ class Feedback extends Component {
     const { good, neutral, bad } = this.state;
     const total = this.countTotalFeedback();
     const positivePercentage = this.countPositiveFeedbackPercentage();
+
     const element = !total ? (
       <p>No feedback given</p>
     ) : (
-      <>
-        <Statistic
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positivePercentage={positivePercentage}
-        />
-      </>
+      <Statistic
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        positivePercentage={positivePercentage}
+      />
     );
 
     return (
       <>
-        <div>
-          <h3 className={styles.title}>Please leave feedback</h3>
-          <button
-            onClick={() => this.changeState('good')}
-            className={styles.btn}
-          >
-            Good
-          </button>
-          <button
-            onClick={() => this.changeState('neutral')}
-            className={styles.btn}
-          >
-            Neutral
-          </button>
-          <button
-            onClick={() => this.changeState('bad')}
-            className={styles.btn}
-          >
-            Bad
-          </button>
-          <p className={styles.stat}> Statistic </p>
-        </div>
-        {element}
+        <FeedbackBlock title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.changeState}
+          />
+        </FeedbackBlock>
+
+        <FeedbackBlock title="Statistic">{element}</FeedbackBlock>
       </>
     );
   }
